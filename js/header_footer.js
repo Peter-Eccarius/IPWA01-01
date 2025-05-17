db0b1b2f294a3c31675b21cf58fa01519b0e2a43
Peter Eccarius <peter.eccarius@gmail.com>
2025-05-17 11:16:30 +0200

    Responsive Design und Code-Refactoring

--- js/header_footer.js
+++ js/header_footer.js
@@ -1,6 +1,6 @@
     fetch("header.html") //holt die Datei header.html
     .then(response => response.text()) //Result wird als Text interpretiert
-    .then(data => {document.getElementById("header").innerHTML = data}); //data ist der reine Inhalt und wird in das Element "header" alt html importiert
+    .then(data => {document.getElementById("header").innerHTML = data}); //data ist der reine Inhalt und wird in das Element "header" als html importiert
 
     fetch("footer.html")
     .then(response => response.text())
